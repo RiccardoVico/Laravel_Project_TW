9cebbd07ab4034b9d200b98e80636a0abed971ba
User <User@WINDELL-2GRUB6I.home>
2022-05-23 19:01:17 +0200

    prova2

--- resources/views/chi_siamo.blade.php
+++ resources/views/chi_siamo.blade.php
@@ -145,6 +145,15 @@
                 </div>
               </div>
             </div>
+             <center>
+    <div class="page-section">
+              <div class="container">
+                <div class="text-center">
+                  <div class="subhead">Dove siamo</div>
+                  <h2 class="title-section">Vieni a trovarci nella nostra Università</h2>
+                </div>
+        <iframe width="638" height="525" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.it/maps?f=q&amp;source=s_q&amp;hl=it&amp;geocode=&amp;q=Via+Brecce+Bianche,+12,+Ancona&amp;aq=0&amp;sll=41.442726,12.392578&amp;sspn=23.377375,53.657227&amp;ie=UTF8&amp;hq=&amp;hnear=Via+Brecce+Bianche,+12,+60131+Ancona,+Marche&amp;z=14&amp;ll=43.581248,13.515684&amp;output=embed"></iframe><br /><small><a href="http://maps.google.it/maps?f=q&amp;source=embed&amp;hl=it&amp;geocode=&amp;q=Via+Brecce+Bianche,+12,+Ancona&amp;aq=0&amp;sll=41.442726,12.392578&amp;sspn=23.377375,53.657227&amp;ie=UTF8&amp;hq=&amp;hnear=Via+Brecce+Bianche,+12,+60131+Ancona,+Marche&amp;z=14&amp;ll=43.581248,13.515684" style="color:#0000FF;text-align:left">Ingrandisci</a></small>
+    </center>
         </main>
 
         <footer class="page-footer">
