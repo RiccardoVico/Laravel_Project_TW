e0c6da217a1d98af0ffb84140a879efa671d1fc7
Riccardo Vico <s1094005@studenti.univpm.it>
2022-06-09 04:41:53 +0200

    aggiunta placeholder e fix messaggistica

--- resources/views/messaggistica.blade.php
+++ resources/views/messaggistica.blade.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
         <link rel="stylesheet" href="{{ asset('css/theme.css') }}">
         <link rel="stylesheet" href="{{ asset('css/registration_form.css') }}">
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
 
     </head>
@@ -36,7 +36,7 @@
                                         @foreach ($utenti as $utente)
                                         <a href="{{ route('messaggistica', [Auth::user()->id, $utente->id]) }}">
                                             <li class="clearfix" >
-                                                <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="avatar">
+                                                <img src="../../images/person/download.jpg" alt="avatar">
                                                 <div class="about">
                                                     <div class="name">{{ $utente->nome }} {{ $utente->cognome }}</div>
                                                 </div>
@@ -52,20 +52,12 @@
                                         <div class="row">
                                             <div class="col-lg-6">
                                                 <a href="javascript:void(0);" data-toggle="modal" data-target="#view_info">
-                                                    <img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="avatar">
+                                                    <img src="../../images/person/download.jpg" alt="avatar">
                                                 </a>
                                                 <div class="chat-about">
                                                     <h6 class="m-b-0">
                                                         {{ $destinatario->nome }} {{ $destinatario->cognome }}
-                                                        {{-- @if ($messaggi->first()->idutente1!=Auth::user()->id)
-                                                        {{ $utenti->where('id', $messaggi->first()->idutente1)->first()->nome }}
-                                                        {{ $utenti->where('id', $messaggi->first()->idutente1)->first()->cognome }}
-                                                        @else
-                                                        {{ $utenti->where('id', $messaggi->first()->idutente2)->first()->nome }}
-                                                        {{ $utenti->where('id', $messaggi->first()->idutente2)->first()->cognome }}
-                                                        @endif --}}
                                                     </h6>                                                    
-                                                    {{-- <small>Last seen: 2 hours ago</small> --}}
                                                 </div>
                                             </div>
                                         </div>
@@ -132,86 +124,33 @@
                                     <div class="chat-header clearfix">
                                         <div class="row">
                                             <div class="col-lg-6">
-                                                {{-- <a href="javascript:void(0);" data-toggle="modal" data-target="#view_info">
-                                                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="avatar">
-                                                </a> --}}
-                                                {{-- <div class="chat-about">
-                                                    <h6 class="m-b-0">{{ $utente->nome }} {{ $utente->cognome }}</h6>
-                                                    {{-- <small>Last seen: 2 hours ago</small> 
-                                                </div> --}}
+                                            </div>
                                         </div>
-                                        {{-- <div class="col-lg-6 hidden-sm text-right">
-                                        <a href="javascript:void(0);" class="btn btn-outline-secondary"><i class="fa fa-camera"></i></a>
-                                    <a href="javascript:void(0);" class="btn btn-outline-primary"><i class="fa fa-image"></i></a>
-                                    <a href="javascript:void(0);" class="btn btn-outline-info"><i class="fa fa-cogs"></i></a>
-                                    <a href="javascript:void(0);" class="btn btn-outline-warning"><i class="fa fa-question"></i></a>
-                                            </div> --}}
+                                    </div>
+                                    <div class="chat-history">
+                                        <div class="place_holder">
+                                            <span>Seleziona una chat per visualizzarla</span>
+                                        </div>
+                                    </div>
+                                    @endif
+                                    @endif
                                 </div>
                             </div>
-                            <div class="chat-history">
-                                <div class="place_holder">
-                                <span>Seleziona una chat per visualizzarla</span>
                         </div>
-                        {{-- <li class="clearfix">
-                                                <div class="message-data text-right">
-                            <span class="message-data-time">10:10 AM, Today</span>
-
-                       </div>
-                            <div class="m                            essage other-message float-right"> Hi Aiden, how are you? How is the project coming along? </div>
-                                        </li>
-                                                        <li class="clearfix">
-                                <div class="                                message-data">
-                                    <span class="message-dat                                    a-time">10:12 AM, Today</span>
-                                                </div>
-                                                <div class="message my-message">Are we meeting today?</div>                                    
-                                            </li>                               
-                                            <li class="clearfix">
-                                                <div class="message-data">
-                                            <span class="message-data-time">10:15 AM, Today</span>
-                                                </div>
-                                                <div class="message my-message">Project has been already finished and I have results to show you.</div>
-                                            </li>
-                                                    <li class="clearfix">
-                                                        <div class="message-data">
-                                                            <span class="message-data-time">10:18 AM, Today</span>
-                                                        </div>
-                                                        <div class="message my-message">Dio Cane</div>
-                                                    </li>
-                                            <li class="clearfix">
-                                                <div class="message-data text-right">
-                                            <span class="message-data-time">10:20 AM, Today</span>
-
-               </div>
-                                                                     <div class="message other-message float-right"> Porc                                        a Madonna </div>
-                            </li> --}}
-                        </div>
-                        {{-- <                                    div class="chat-message clearfix">
-                                        <div class="input-group mb-0">
-                                            <div class="input-group-prepend">
-                        <span class="input-group-text"><i class="fa fa-send"></i></span>
-                                            </div>
-                                                <input type="text" class="form-control" placeholder="Enter text here...">                                    
+                    </div>
+                </div>
             </div>
-        <                    /div> --}}
-        @endif
-        @endif
-    </                div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</main>
+        </main>
 
-<footer class="page-footer">
-    @include('layouts/footer_public')
-</footer>
-<!-- end #footer -->
+        <footer class="page-footer">
+            @include('layouts/footer_public')
+        </footer>
+        <!-- end #footer -->
 
-{{-- <script src="../js/profilo_locatario.js"></script> --}}
-<script src="../js/jquery-3.5.1.min.js"></script>
-<script src="../js/bootstrap.bundle.min.js"></script>
-<script src="../js/theme.js"></script>
+        {{-- <script src="../js/profilo_locatario.js"></script> --}}
+        <script src="../js/jquery-3.5.1.min.js"></script>
+        <script src="../js/bootstrap.bundle.min.js"></script>
+        <script src="../js/theme.js"></script>
 
-</body>
+    </body>
 </html>
